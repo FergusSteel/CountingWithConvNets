96a144606f621a5e77c87ff9ce2925948f14d56f
FergusSteel <fergus.k.steel@gmail.com>
2023-11-24 19:23:20 +0000

    Added PytTorch Dataset functionality

---
+++ src/torchdataset.py
@@ -0,0 +1,36 @@
+import  os
+import torch
+import numpy as np
+import pilot_utils
+import convert_to_npz
+from torch.utils.data import Dataset, DataLoader
+import warnings
+import matplotlib.image as mpimg
+
+warnings.filterwarnings("ignore")
+
+
+class SpreadMNISTDataset(Dataset):
+    def __init__(self, num_points, transform=None):
+        self.transform = transform
+        self.dmaps = convert_to_npz.load_data(num_points)
+    
+
+    def __len__(self):
+        return len(self.dmaps)
+    
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_path = os.path.join(os.path.abspath(".."), "data", "train", "x")
+        image = mpimg.imread(os.path.join(img_path,f"{idx}.png"))
+        dmap = self.dmaps[idx]
+
+        sample = {'image': image, 'dmap': dmap}
+
+        if self.transform:
+            sample = self.transform(sample)
+        
+        return sample
+    
