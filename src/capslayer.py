23aaf99c9cc6e0dd61e0c74d0efde9f13fbeb5d2
FergusSteel <fergus.k.steel@gmail.com>
2023-11-24 19:25:15 +0000

    Added source code files for capsnet

---
+++ src/capslayer.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+class CapsLayer(nn.Module):
+    def __init__(self, ):
+        super().__init__()
