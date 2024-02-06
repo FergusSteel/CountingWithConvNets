1c25f7d382d481b00ca8a7c0716ff2b44830025f
Fergus Steel <fergus.k.steel@gmail.com>
2024-02-06 10:25:28 +0000

    Cleaned some code in main

--- src/capsmain.py
+++ src/capsmain.py
@@ -2,7 +2,7 @@
 
 import os
 import torch
-from capsnet import SegCaps, CapsNetBasic
+from capsnet import SegCaps, CapsNetBasic, CCCaps
 from torch.utils.data import DataLoader
 from torchdataset import *
 from torchdataset_masks import *
@@ -13,18 +13,18 @@ from pilot_utils import show_density_map, load_batch
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def init(args):
-    print("=================FLAGS==================")
-    for k, v in args.__dict__.items():
-        print('{}: {}'.format(k, v))
-    print("========================================")
     os.environ['CUDA_VISIBLE_DEVICES'] =args.gpu
     print('{}:{}'.format('cuda',torch.cuda.is_available()))
     args.cuda = torch.cuda.is_available()
     if args.cuda:
         torch.cuda.manual_seed(args.seed)
-    train_loader = DataLoader(SpreadMNISTDataset(args.n_images), batch_size=args.batch_size_train, shuffle=True)
-    test_loader = DataLoader(SpreadMNISTDataset(int(args.n_images*0.2), train=False), batch_size=args.batch_size_train, shuffle=True)
-    model=SegCaps()
+    train_loader = DataLoader(SpreadMNISTDataset(args.n_images), batch_size=args.batch_size_train, shuffle=False)
+    test_loader = DataLoader(SpreadMNISTDataset(int(args.n_images*0.2), train=False), batch_size=args.batch_size_train, shuffle=False)
+    if args.model == "Seg":
+        model=SegCaps()
+    elif args.model == "CCC":
+        model = CCCaps()
+
     model.cuda()
     model.to(device)
     
@@ -43,30 +43,24 @@ def show_n_example(model, n):
         print("-"*10)
         print("True count = ", round(sum(sum(sum(dat[1][i]))) / 1000))
         print("Total count = ", round(sum(sum(sum(outputs.cpu().detach().numpy()[0]))) / 1000))
-        print(dat[1][i].shape)
-        print(inputs.shape)
-        print(outputs.shape)
         for j in range(10):
-             print(f"{j}'s... True Count = {round(sum(sum(dat[1][i][j])) / 1000)}, Predicted Count = {sum(sum(outputs.cpu().detach().numpy()[0][j])) / 1000}")
+             print(f"{j}'s... True Count = {round(sum(sum(dat[1][i][j])) / 1000)}, Predicted Count = {round(sum(sum(outputs.cpu().detach().numpy()[0][j])) / 1000)}")
              show_density_map(inputs[i][0].cpu().detach().numpy(), outputs[0][j].cpu().detach().numpy())
         
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Little Ma's train")
     parser.add_argument('--batch_size_train', type=int, default=1, help='input batch size for training (default: 160)')
-    parser.add_argument('--batch_size_test', type=int, default=1, help='input batch size for testing (default: 80)')
+    parser.add_argument('--model', default="Seg", help='Which model to use (default: Seg)')
     parser.add_argument("--n_images", type=int, default=1000, help="Number of images to train on.")
     parser.add_argument('--lr', type=float, default=0.001, help='learning rate (default: 1e-3)')
-    parser.add_argument('--gpu', default='0', help='index of gpus to use')
     parser.add_argument('--dlr', default='10,25', help='decreasing strategy')
-    parser.add_argument('--model', default='segcaps-train', help='which model (default: xception)')
-    parser.add_argument('--data_root', default='./', help='data_root (default: ./)')
     parser.add_argument('--nepoch', type=int,default=1, help='epochs (default: 200)')
     parser.add_argument('--seed', type=int,default='10', help='seed (default: 1)')
     parser.add_argument('--pretrain', type=int, default='0', help='pretrain (default: 1)')
     parser.add_argument('--data_name', default='train', help='data_name (default: train)')
     parser.add_argument('--params_name', default='segcaps.pkl', help='params_name (default: segcaps.pkl)')
-    parser.add_argument('--load_params_name', default='segcaps_best_so_far.pkl', help='params_name (default: segcaps.pkl)')
+    parser.add_argument('--load_params_name', default='segcaps.pkl', help='params_name (default: segcaps.pkl)')
     args = parser.parse_args()
 
     if args.pretrain == 1:
@@ -83,7 +77,5 @@ if __name__ == '__main__':
         train(args,model,train_loader,test_loader,
                 decreasing_lr,wd=0.0001, momentum=0.9)
 
-    show_n_example(model, 10)
-    print('hhh')
-    print('Done!')
+    show_n_example(model, 3)
 
